f23b8ed531ff7f56d42d0d1aff2defba7eacaa57
Aaron <aaturner1995@gmail.com>
2022-12-28 14:53:51 -0400

    adding javascript

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var i=0; i<topic.length; i++) {
+    console.log(topic[i]);
+}
+
+/*
+if (topic === `HTML`) {
+    console.log("Let's Study HTML!");
+}
+else if (topic === `CSS`) {
+    console.log("Let's Study CSS!");
+}
+else if (topic === `Git`) {
+    console.log("Let's Study Git!");
+}
+else if (topic === `JavaScript`) {
+    console.log("Let's Study JavaScript!");
+}
+else {
+    console.log("Please Try Again!")
+}
+*/
